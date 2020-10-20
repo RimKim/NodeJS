6479afc2d543b1d5f907c68db47b877a63102f01
RimKim <rim.kim92@gmail.com>
2020-10-20 21:30:39 +0200

    login functionality and token

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 4000
 const bodyParser = require('body-parser')
-
+const cookieParser = require('cookie-parser');
 const config = require('./config/key');
 
 const { User } = require("./models/User");
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI, {
@@ -33,6 +34,34 @@ app.post('/register', (req, res) => {
     if(err) return res.json({success: false, err})
     return res.status(200).json({
       success: true
+    })
+  })
+})
+
+app.post('/login', (req, res) => {
+
+  // check if the email is in db
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "No user found"
+      })
+    }
+    // check if the password is correct
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message: "Password is not correct" })
+
+      // generate token
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+
+        // save token in Cookies using cookie-parser (or Local Storage)
+        res.cookie("x_auth", user.token)
+        .status(200)
+        .json({ loginSuccess: true, userId: user._id })
+      })
     })
   })
 })
--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -36,7 +37,7 @@ const userSchema = mongoose.Schema({
 userSchema.pre('save', function( next ){
 
     var user = this;
-    
+
     if(user.isModified('password')) {
         // encode password using salt
         bcrypt.genSalt(saltRounds, function(err, salt) {
@@ -52,6 +53,31 @@ userSchema.pre('save', function( next ){
         next()
     }
 })
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+
+    //check if the plain password equals the encoded password
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch) {
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb) {
+
+    var user = this;
+    
+    // generate token using jwt
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    // user._id + 'secretToken' = token
+    // token -> 'secretToken' -> user._id 
+
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
